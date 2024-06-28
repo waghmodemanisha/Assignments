36f1b488da561fac486ac10976beb8a7fc0f0442
waghmodemanisha <31162242+waghmodemanisha@users.noreply.github.com>
2024-06-28 12:01:24 +0530

    changes updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,40 +34,10 @@ const forms = document.querySelector(".forms"),
 
         }
       }
-
-
-
-
-
       
 links.forEach(link => {
     link.addEventListener("click", e => {
-       e.preventDefault(); //preventing form submit
+       e.preventDefault();
        forms.classList.toggle("show-signup");
     })
  });
-
-
- $(document).on("click", ".browse", function() {
-  var file = $(this).parents().find(".file");
-  file.trigger("click");
-});
-$('input[type="file"]').change(function(e) {
-  var fileName = e.target.files[0].name;
-  $("#file").val(fileName);
-
-  var reader = new FileReader();
-  reader.onload = function(e) {
-    // get loaded data and render thumbnail.
-    document.getElementById("preview").src = e.target.result;
-  };
-  // read the image file as a data URL.
-  reader.readAsDataURL(this.files[0]);
-});
-// $(function () {
-//   $('.datepicker').datepicker({
-//     language: "es",
-//     autoclose: true,
-//     format: "dd/mm/yyyy"
-//   });
-// });
